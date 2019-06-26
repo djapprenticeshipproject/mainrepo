f2e509211a656b01f1c13f34721b2215c8dad6ff
DBstandalone <DBstandalone@outlook.com>
2019-06-26 11:18:39 +0100

    first commit of main.java and msExcel.java

---
+++ djapprenticeshipproject/src/djapprenticeshipproject/main.java
@@ -0,0 +1,10 @@
+package djapprenticeshipproject;
+
+public class main {
+
+	public static void main(String[] args) {
+		// all the main methods will go here later
+
+	}
+
+}
---
+++ djapprenticeshipproject/src/djapprenticeshipproject/msExcel.java
@@ -0,0 +1,5 @@
+package djapprenticeshipproject;
+
+public class msExcel {
+
+}
